b63d3a301f928026dd99400b0c74a2554a016a46
aaron levin <aaron.levin@soundcloud.com>
2016-02-04 12:18:24 +0100

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ mkDerivation, aeson, base, containers, either, http-types, lens
+, network-uri, QuickCheck, quickcheck-instances, scientific
+, servant, servant-client, servant-mock, servant-server, split
+, stdenv, text, transformers, unordered-containers, vector, wai
+, warp
+}:
+mkDerivation {
+  pname = "haskell-kubernetes";
+  version = "0.1.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    aeson base containers either http-types lens network-uri QuickCheck
+    quickcheck-instances scientific servant servant-client
+    servant-server split text unordered-containers vector wai
+  ];
+  executableHaskellDepends = [
+    base either network-uri QuickCheck servant servant-client
+    servant-mock servant-server split transformers warp
+  ];
+  homepage = "https://github.com/soundcloud/haskell-kubernetes";
+  description = "Haskell bindings to the Kubernetes API (via swagger & servant)";
+  license = stdenv.lib.licenses.bsd3;
+}
